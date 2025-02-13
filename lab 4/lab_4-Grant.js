d4a2ad83b7bc3f7973f1dda129c3333b570cef10
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-02-13 15:28:45 -0500

    Update lab_4-Grant.js

--- lab 4/lab_4-Grant.js
+++ lab 4/lab_4-Grant.js
@@ -51,3 +51,37 @@ function checktemp(temperature){
 let t = 100
 let temp_result = checktemp(t)
 console.log(`Is this temperature greater than 75? ${temp_result}`)
+
+console.log(`---------- Example 7: Built-in Functions ----------`)
+//Math object
+let num_pi = Math.PI
+console.log(`JS PI value = ${num_pi}.`)
+//round() rounds to the nearest integer
+console.log(`Rounded Up PI = ${Math.round(num_pi)}.`)
+//ceil() rounds up to next number
+console.log(`Ceil Method applied to PI = ${Math.ceil(num_pi)}.`)
+//pow() does exponents
+console.log(`2^3 = ${Math.pow(2,3)}.`)
+//sqrt() does the square root of a number
+console.log(`Square Root of 20 = ${Math.sqrt(20)}.`)
+//random() randomizes a number between 0 and 1
+console.log(`Random Number = ${Math.random()}.`)
+
+let n = 1 + Math.round(Math.random()*8)
+console.log(`A number between 1 and 9 is ${n}.`)
+
+console.log(`---------- Example 8: Random Number ----------`)
+function randnum(){
+    let p = -5 + Math.round(Math.random()*10)
+    return p;
+}
+
+console.log(`---------- Example 9: Random Number in a List (Array) ----------`)
+let colors = ["red", "green", "blue", "magneta", "gray"]
+function randclr(){
+    let list_size = colors.length
+    let last_index = list_size - 1
+    let random_index = Math.round(Math.random()*last_index)
+    return colors[random_index]
+}
+console.log(`Color picked = ${randclr()}.`)
