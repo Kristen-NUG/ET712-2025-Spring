d5c792ddfa4791df1525d62b9be45df79c279eb2
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-02-13 17:54:30 -0500

    commit

--- ET712 hw_2/Grant_hw2.js
+++ ET712 hw_2/Grant_hw2.js
@@ -4,5 +4,13 @@ console.log(`Kristen Grant ET712 Homework 2`)
 
 console.log(`---------- Program 1: Name_Counting Function ----------`)
 function name_counting(){
-    
+    let names = ['Eleanor', 'Penelope', 'Daisy', 'Lorie', 'Aj', 'Sam', 'Theodore', 'Ella', 'Bella', 'Amber', 'Steph', 'Elizabeth', 'Olivander', 'Nathan', 'Courtney']
+    let count = 0;
+    for (let i = 0; i < names.length; i++){
+        if (names[i].length <= 5){
+            count++;
+        }
+    }
+    console.log(`These are the list of names: ${names}.`)
+    console.log(`There are ${count} names with 5 or fewer characters in the array.`)
 }
