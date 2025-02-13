c2d112b579fc08a9fae05288017d14cdbb5cdcaf
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-02-13 17:18:25 -0500

    commit

---
+++ ET712 hw_2/Grant_hw2.js
@@ -0,0 +1,8 @@
+/*Homework 2: Control Flows, Loops, and Functions
+Student Name: Kristen Grant*/
+console.log(`Kristen Grant ET712 Homework 2`)
+
+console.log(`---------- Program 1: Name_Counting Function ----------`)
+function name_counting(){
+    
+}
