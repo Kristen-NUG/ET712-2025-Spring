40bd84126b6abbfab84edda25bdc3a176a590314
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-02-20 14:38:29 -0500

    a

--- lab 5/lab5_Grant.js
+++ lab 5/lab5_Grant.js
@@ -1,3 +1,13 @@
 console.log(`Kristen Grant`)
 console.log(`Lab 5`)
-console.log(`---------- Example 1: ----------`)
+console.log(`---------- Example 1: Function in a Variable ----------`)
+const sum = function(num1, num2){
+    return num1 + num2
+}
+console.log(sum(5, 8))
+
+console.log(`---------- Example 2: Function in a Variable ----------`)
+let squarenumber = function(n){
+    return Math.pow(n,2)
+}
+console.log(squarenumber(5))
