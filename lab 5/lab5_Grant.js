a69363040ea597a6d8191f47c1200efdc61fcae7
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-02-20 14:55:38 -0500

    a

--- lab 5/lab5_Grant.js
+++ lab 5/lab5_Grant.js
@@ -11,3 +11,21 @@ let squarenumber = function(n){
     return Math.pow(n,2)
 }
 console.log(squarenumber(5))
+
+console.log(`---------- Example 3: Arrow Function ----------`)
+let greet = function(username){
+    console.log(`Welcome to JS ${username}`)
+}
+greet(`Kristen`)
+
+console.log(`---------- Example 4: Function with Default Parameter ----------`)
+function cubenumber(n=1){
+    return Math.pow(n,3)
+}
+console.log(`2^3 = ${cubenumber(2)}`)
+console.log(`empty^3 = ${cubenumber()}`)
+
+console.log(`---------- Example 5: Spread Syntax ----------`)
+nums = [-2, 5, 9, 3, -8]
+let maxnum = Math.max(...nums)
+console.log(maxnum)
