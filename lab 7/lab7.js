f048d642ffdac85c50a53db467bb757e387b43f5
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-03-04 15:51:40 -0500

    add to lab 7

--- lab 7/lab7.js
+++ lab 7/lab7.js
@@ -17,3 +17,23 @@ let btn = document.querySelector(".btnclick")
 btn.addEventListener("click", function(){
     alert(`BTN WAS CLICKED!`)
 })
+console.log(`-------- Example 4: The Change Event --------`)
+let btnmsg = document.querySelector(".btnmsg")
+let msg = document.querySelector(".msg")
+btnmsg.addEventListener("click", function(){
+    msg.innerHTML = "Good afternoon <b> Kristen Grant</b>"
+})
+console.log(`-------- Example 5: Changing Shapes Mini-App --------`)
+let shape = document.querySelector(".shape")
+let btncircle = document.querySelector(".btncircle")
+let btnsquare = document.querySelector(".btnsquare")
+let btnrectangle = document.querySelector(".btnrectangle")
+btncircle.addEventListener("click", function(){
+    shape.className = ("circle")
+})
+btnsquare.addEventListener("click", function(){
+    shape.className = ("square")
+})
+btnrectangle.addEventListener("click", function(){
+    shape.className = ("rectangle")
+})
