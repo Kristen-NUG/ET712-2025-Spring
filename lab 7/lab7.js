d17e21ead83e45b720c0676f1667e84d0c92c1e3
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-03-04 14:55:38 -0500

    add to  lab 7

---
+++ lab 7/lab7.js
@@ -0,0 +1,7 @@
+console.log(`Kristen Grant`)
+let desc = document.querySelector(".description");
+console.log(desc)
+let titlenode = document.querySelector("#title");
+console.log(titlenode)
+let par = document.querySelectorAll("p");
+console.log(par)
