8358504a99a7d3cf07490385c9f59d0b3e3c7370
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-03-04 15:06:53 -0500

    add to lab 7.js

--- lab 7/lab7.js
+++ lab 7/lab7.js
@@ -1,7 +1,14 @@
 console.log(`Kristen Grant`)
+console.log(`-------- Example 1: Selecting Elements --------`)
 let desc = document.querySelector(".description");
 console.log(desc)
 let titlenode = document.querySelector("#title");
 console.log(titlenode)
 let par = document.querySelectorAll("p");
 console.log(par)
+let methods = document.querySelectorAll(".method");
+console.log(methods)
+console.log(`-------- Example 2: Loop Through Each Element in a Node --------`)
+for(let n = 0; n<methods.length; n++){
+    console.log(methods[n]);
+}
